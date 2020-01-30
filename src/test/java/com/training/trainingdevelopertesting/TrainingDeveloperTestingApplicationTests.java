44bd14d83e21aae16d55bc611edfc870d4fc1a9d
Anant Pawar <anant.c.pawar@gmail.com>
2020-01-30 21:27:48 +0530

    inital commit

---
+++ src/test/java/com/training/trainingdevelopertesting/TrainingDeveloperTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.trainingdevelopertesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingDeveloperTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
